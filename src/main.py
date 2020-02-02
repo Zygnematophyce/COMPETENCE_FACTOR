56b68463299898f123073928ce5ac0d8184b827b
lfourteau91@gmail.com <lfourteau91@gmail.com>
2020-02-02 20:37:24 +0100

    implement cairo

--- src/main.py
+++ src/main.py
@@ -1,20 +1,49 @@
 # coding: utf-8
 
-# using Gtk librairie
+# Using Gtk 3.0 library
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+import cairo
 
-from gi.repository import Gtk
+def draw_polygone(widget, cairo):
+    """ Drawing polygone with cairo. """
+    cairo.move_to(30, 30)
+    cairo.line_to(30, 55)
+    cairo.line_to(60, 55)
+    cairo.line_to(30, 70)
+    cairo.stroke_preserve()
+    cairo.set_source_rgb(1, 0, 0)
+    cairo.set_line_width(5)
+    cairo.stroke()
+    return False
 
 if __name__ == "__main__":
     print("Competence factor")
 
-    # Create windows
+    # Create window
     window = Gtk.Window()
+
+    # Set title of window
+    window.set_title("Competence Factor")
+
+    # Set size of window
+    window.set_size_request(400, 550)
+
+    # Centering the window
+    window.set_position(Gtk.WindowPosition.CENTER)
 
     # Add destroy button to quit window
     window.connect("destroy", Gtk.main_quit)
+
+    # Create a drawing area
+    drawing_area = Gtk.DrawingArea()
+
+    # Add drawing area
+    drawing_area.connect('draw', draw_polygone)
+
+    # Add drawing area into the window
+    window.add(drawing_area)
 
     # Show all
     window.show_all()

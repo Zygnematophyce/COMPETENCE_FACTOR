6e7a7f817465241b957d461f73aff32f7b093ed4
collaborateur <lfourteau91@gmail.com>
2020-10-02 00:14:32 +0200

    Possibility to increase skill with start plot.

--- src/main.py
+++ src/main.py
@@ -15,97 +15,94 @@ class MainWindow(tk.Tk):
 
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.title("Competence Factor")
+        self.title("Skill Factor")
         self.width = 500
         self.height = 550
         self.geometry("{}x{}".format(self.width,
                                      self.height))
-                
-        
-
-#class FrameWidget(tk.Tk):
     
 
 if __name__ == "__main__":
     print("competence factor")
 
+    # Create a window tkinter object.
     window = MainWindow()
 
+    # Create a Frame.
     main_frame = tk.Frame(window)
-    main_canvas = tk.Canvas(main_frame, bg="ivory", width=500, height=550)
 
-    pos_x = window.width/2
-    pos_y_init = 5
-    long_line = 210
+    # Create a Canvas.
+    main_canvas = tk.Canvas(main_frame,
+                            bg="ivory",
+                            width=500,
+                            height=550)
 
-    # Create the main line.
-    main_canvas.create_line(pos_x,
-                            pos_y_init,
-                            pos_x,
-                            long_line)
+    # Define the number of skill.
+    number_skill = 5
+    print("Number of skill : ", number_skill)
 
-    pos_y = pos_y_init
+    # Define length of main line.
+    length_main_line = 210
 
-    # Create 10 transverses lines.
-    for i in range(0, 11):
-        main_canvas.create_line(pos_x - 5,
-                                pos_y,
-                                pos_x + 5,
-                                pos_y)
+    # The length of transverse line.
+    len_transv_line = 5
 
-        pos_y = long_line/10*i
-
-
-    # Add trigonometric.
-    x_center = window.width/2
-    y_center = long_line
+    # Centering the origin (default top-left).
+    origin_x = window.width/2
+    origin_y = length_main_line
     
-    degres = 10
-    radian = math.radians(degres)
-
-    # x = cos(radian)*r
-    x_new = math.cos(radian)*long_line
-    
-    # y = sin(radian)*r
-    y_new = math.sin(radian)*long_line
-
-    # Centering the x and y points.
-    x_new = x_new + x_center
-    y_new = y_new + y_center
-
-    main_canvas.create_line(x_center,
-                            y_center,
-                            x_new,
-                            y_new)
+    # Define the degres.
+    degres = 360/number_skill
+    print(degres)
 
     # The degrees to be subtracted from the degree
     # of the main line.
     radian_substract = math.radians(90)
-    
-    # Create 10 transverses lines.
-    for i in range(1, 11):
-        # Adding all points centered on the main line.
-        try:
-            point_x_center = ((long_line * (10*i) /100) * math.cos(radian)) + x_center
-            point_y_center = ((long_line * (10*i) /100) * math.sin(radian)) + y_center
-        except ZeroDivisionError as e:
-            print(e)
-            point_x_center = x_center
-            point_y_center = y_center
 
-        # Create the first points of line.
-        xa_coord_line = 5 * math.cos(radian - radian_substract) + point_x_center
-        ya_coord_line = 5 * math.sin(radian - radian_substract) + point_y_center
+    # Distribution around the circle of lines.
+    for i in range(0, number_skill):
 
-        # Create the second points of line.
-        xb_coord_line = 5 * -1 * math.cos(radian - radian_substract) + point_x_center
-        yb_coord_line = 5 * -1 * math.sin(radian - radian_substract) + point_y_center
+        # Define radian.
+        radian = math.radians(degres)
+
+        # Adding the points for the main line.
+        x_main_line = math.cos(radian)*length_main_line + origin_x
+        y_main_line = math.sin(radian)*length_main_line + origin_y
+
+        # Plot main line.
+        main_canvas.create_line(origin_x,
+                                origin_y,
+                                x_main_line,
+                                y_main_line)
+
+        # Increment degres
+        degres = degres + (360/number_skill)
+        print(degres)
     
-        # Plot all transverses lines.
-        main_canvas.create_line(xa_coord_line,
-                                ya_coord_line,
-                                xb_coord_line,
-                                yb_coord_line)
+        # Create 10 transverses lines.
+        for y in range(1, 11):
+            # Adding all points centered on the main line.
+            try:
+                point_x_center = ((length_main_line * (10*y) /100) * math.cos(radian)) + origin_x
+                point_y_center = ((length_main_line * (10*y) /100) * math.sin(radian)) + origin_y
+            except ZeroDivisionError as e:
+                print(e)
+                point_x_center = origin_x
+                point_y_center = origin_y
+
+            # Create the first points of line.
+            xa_coord_line = len_transv_line * math.cos(radian - radian_substract) + point_x_center
+            ya_coord_line = len_transv_line * math.sin(radian - radian_substract) + point_y_center
+
+            # Create the second points of line.
+            xb_coord_line = len_transv_line * -1 * math.cos(radian - radian_substract) + point_x_center
+            yb_coord_line = len_transv_line * -1 * math.sin(radian - radian_substract) + point_y_center
+
+            # Plot all transverses lines.
+            main_canvas.create_line(xa_coord_line,
+                                    ya_coord_line,
+                                    xb_coord_line,
+                                    yb_coord_line)        
 
     main_frame.pack()
     main_canvas.pack()
